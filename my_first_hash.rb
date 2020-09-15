85e126f7204e304a817a27e9a984f999f900fb48
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-15 23:30:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,4 @@
-def my_hash(your_hash)
+def my_hash
   your_hash = {"my_first_key" => "my first pair"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
